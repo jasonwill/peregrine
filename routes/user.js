dcea73c095352966d4c4763426160e14eed81371
Jason Williams <jwilliams@pnca.edu>
2013-07-15 15:38:25 -0700

    add avy

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 var app = require('../app');
 
 exports.find_by_username = function(req, res){	
-	app.sendQuery('SELECT id, login, name, email, color FROM users WHERE login = ?', [req.params.username], function(results) {
+	app.sendQuery('SELECT id, login, name, email, color, avatar_url FROM users WHERE login = ?', [req.params.username], function(results) {
     res.send(results);
     
 	  //res.render('user', results);
